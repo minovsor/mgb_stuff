65e752ac82014eb82ff8b7845e0a368a1b0fd103
minovsor <minovsor@users.noreply.github.com>
2024-08-13 10:25:56 -0300

    Add files via upload

---
+++ le_qobs.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+LEITURA QOBS
+
+@author: MINO SORRIBAS
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime,timedelta
+
+# ARQUIVO COM VAZOES OBSERVADAS
+fil_qobs = 'QOBS.qob'
+with open(fil_qobs,'r') as f:
+    lidos = f.readlines()
+    header_0 = lidos[0].split()
+    dados = []
+    for i in lidos[1:]:
+        line = i.split()
+        dia,mes,ano = list(map(int,line[:3]))
+        valores = list(map(float,line[3:]))
+        dados.append([datetime(ano,mes,dia)] + valores)
+    header = ['datetime'] + header_0[3:]
+    df_qobs = pd.DataFrame(dados, columns=header)
+    df_qobs = df_qobs.set_index('datetime')
